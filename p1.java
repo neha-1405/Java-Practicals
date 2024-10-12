e065d01dd126b0d9e09cf26739aa39f63d082c78
neha1405 <164915334+neha-1405@users.noreply.github.com>
2024-10-13 01:24:37 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,9 @@
+public class p1
+{
+    public static void main(String[] args) {
+        int age=25;
+
+        System.out.println(+age+" is the age of Sam.");
+        System.out.println("23DIT047");
+    }
+}
