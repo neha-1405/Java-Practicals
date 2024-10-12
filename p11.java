9e1b5d18c285530d1b63c897e7eb062eb9158223
neha1405 <164915334+neha-1405@users.noreply.github.com>
2024-10-13 01:39:19 +0530

    Add files via upload

---
+++ p11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class p11 {
+public static void main(String[] args) {
+System.out.println("Enter your amount in pounds: ");
+Scanner sc = new Scanner(System.in);
+int pound = sc.nextInt();
+int rupee = pound*100;
+System.out.println("Your amount in rupees is: "+rupee);
+System.out.println("23DIT047");
+}
+}
